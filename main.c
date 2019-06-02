73f99dab54a1af5a340c420c8ab91703a2c05a20
DHKim <ultraraptor@naver.com>
2019-06-02 12:26:01 +0900

    ring buffer add

--- main.c
+++ main.c
@@ -23,6 +23,14 @@ void TX1_Byte(unsigned char data);
 void print(unsigned char *message);
 void UI_Handler(unsigned char *qdata);
 
+
+static unsigned char rbuf_in = 0;
+static unsigned char rbuf_out = 0;
+static const unsigned int MAX_rbuf = 2000;
+static unsigned char rbuf[MAX_rbuf] = {0};
+
+
+
 int main(void)
 {
 
@@ -41,82 +49,89 @@ int main(void)
 
 }
 
+
 void UART0_Init(void)
 {
-    UBRR0H = 0;//baud = 9600!!
-    UBRR0L = 207;//baud = 9600!!
-    UCSR0A = 0x02;                  //asynchronous normal mode
-    UCSR0B = (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0); //interrupt, Rx/Tx enable
-    UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);//no parity, 1 stop, 8 data
+  UBRR0H = 0;//baud = 9600!!
+  UBRR0L = 207;//baud = 9600!
+  UCSR0A = 0x02;                  //asynchronous normal mode
+  UCSR0B = (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0); //interrupt, Rx/Tx enable
+  UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);//no parity, 1 stop, 8 data
 }
 
 void UART1_Init(void)
 {
-	UBRR1H = 0;//baud = 9600!!
-    UBRR1L = 207;//baud = 9600!!
-    UCSR1A = 0x02;                  //asynchronous normal mode
-    UCSR1B = (1<<RXCIE1)|(1<<RXEN1)|(1<<TXEN1); //interrupt, Rx/Tx enable
-    UCSR1C = (1<<UCSZ11)|(1<<UCSZ10);//no parity, 1 stop, 8 data
+  UBRR1H = 0;//baud = 9600!!
+  UBRR1L = 207;//baud = 9600!!
+  UCSR1A = 0x02;                  //asynchronous normal mode
+  UCSR1B = (1<<RXCIE1)|(1<<RXEN1)|(1<<TXEN1); //interrupt, Rx/Tx enable
+  UCSR1C = (1<<UCSZ11)|(1<<UCSZ10);//no parity, 1 stop, 8 data
 }
 
 void UART0_Read_Byte(unsigned char data)
 {
-    while(!(UCSR0A & (1<<UDRE0)));  //wait for empty transmit buffer
-    UDR0 = data;                    //put data into buffer, send the data
+  while(!(UCSR0A & (1<<UDRE0)));  //wait for empty transmit buffer
+  UDR0 = data;                    //put data into buffer, send the data
 }
 
 void UART1_Read_Byte(unsigned char data)
 {
-    while(!(UCSR1A & (1<<UDRE1)));  //wait for empty transmit buffer
-    UDR1 = data;                    //put data into buffer, send the data
+  while(!(UCSR1A & (1<<UDRE1)));  //wait for empty transmit buffer
+  UDR1 = data;                    //put data into buffer, send the data
 }
 
 void print(unsigned char *message)
 {
-    while(*message != '\0'){
-        TX0_Byte(*message);
-        TX1_Byte(*message);
-        message++;
-
-    }
+  while(*message != '\0'){
+    TX0_Byte(*message);
+    TX1_Byte(*message);
+    message++;
+  }
 	
-    while(*message != '\0'){
-        TX1_Byte(*message);
-        message++;
-    }
+  while(*message != '\0'){
+    TX1_Byte(*message);
+    message++;
+  }
 }
 
 void UI_Handler(unsigned char *qdata)
 {
-//QBUFFER
-	if(receive_completion){
-	    receive_completion = 0;
-        switch(receive_data){
-            case '\r' : TX0_Byte('\r'); TX0_Byte('\n');
-                        print(prompt);
-                        break;
-            case '\n' : TX0_Byte('\r'); TX0_Byte('\n');
-                        print(prompt);
-                        break;
-            default   : TX0_Byte(receive_data);
-                        TX0_Byte('\r'); TX0_Byte('\n');
-                        print(prompt);
-                        break;
-        }
+  if(receive_completion){
+    receive_completion = 0;
+    switch(receive_data){
+        case '\r' : TX0_Byte('\r'); TX0_Byte('\n');
+                    print(prompt);
+                    break;
+        case '\n' : TX0_Byte('\r'); TX0_Byte('\n');
+                    print(prompt);
+                    break;
+        default   : TX0_Byte(receive_data);
+                    TX0_Byte('\r'); TX0_Byte('\n');
+                    print(prompt);
+                    break;
     }
-	return;
+  }
+  return;
 }
 
 SIGNAL(SIG_UART0_RECV)
 {
-    receive_data = UDR0;
-    receive_completion = 1;
+  receive_data = UDR0;
+  receive_completion = 1;
 }
 
 
 SIGNAL(SIG_UART1_RECV)
 {
-    	receive_data = UDR1;
-		PORTA = ~PORTA;
-		receive_completion = 1;
+  unsigned char ret = 0;
+  ret = rx_buf[rx_out];
+
+
+  receive_data = UDR1;
+  PORTA = ~PORTA;
+  receive_completion = 1;
 }
+
+
+
+
